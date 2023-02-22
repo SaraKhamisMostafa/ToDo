0282173cea5b3a5ac8a9aa26af5ee63248e3295c
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-02-22 23:37:22 +0200

    edit the design of todo

--- js/code.js
+++ js/code.js
@@ -1,1 +1,17 @@
-console.log('Hello java script')
+const todoInput = document.querySelector(".task-input")
+const todoBtn = document.querySelector(".task-btn")
+const todoResult = document.querySelector(".task-result")
+
+
+function addTodo(e){
+    e.preventDefault();
+    console.log(todoInput.value)
+    
+    todoInput.value=''
+}
+
+
+
+
+
+todoBtn.addEventListener('click' ,addTodo)
