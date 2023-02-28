5958ac76fc6e4fbb5647f873c4f00946b27aa2b2
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-02-28 23:41:45 +0200

    some changes

--- js/code.js
+++ js/code.js
@@ -7,7 +7,21 @@ function addTodo(e){
     e.preventDefault();
     console.log(todoInput.value)
     
-    todoInput.value=''
+    //create li
+    const todoLi = document.createElement("li")
+    todoLi.classList.add("border")
+    todoLi.classList.add("shadow")
+    //create h5
+    const todotitle = document.createElement("h5")
+    todotitle.classList.add("ms-3")
+    todotitle.classList.add("d-inline-block")
+    todotitle.classList.add("w-75")
+    todotitle.innerText = todoInput.value
+    todoLi.appendChild(todotitle)
+
+    todoResult.appendChild(todoLi)
+
+    //todoInput.value=''
 }
 
 
