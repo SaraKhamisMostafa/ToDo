59d7f8827cf549e546cdda6f637652af4d6da7fe
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-03-08 04:32:06 +0200

    add some changes

--- js/code.js
+++ js/code.js
@@ -11,6 +11,9 @@ function addTodo(e){
     const todoLi = document.createElement("li")
     todoLi.classList.add("border")
     todoLi.classList.add("shadow")
+    todoLi.classList.add("my-2")
+    //todoLi.classList.add("uncompleted")
+    
     //create h5
     const todotitle = document.createElement("h5")
     todotitle.classList.add("ms-3")
@@ -19,13 +22,47 @@ function addTodo(e){
     todotitle.innerText = todoInput.value
     todoLi.appendChild(todotitle)
 
+    const todocomplete = document.createElement("button")
+    todocomplete.classList.add("btn")
+    todocomplete.classList.add("btn-primary")
+    todocomplete.classList.add("m-1")
+    todocomplete.innerHTML =`<i class="fa-solid fa-check"></i>`
+    todoLi.appendChild(todocomplete)
+
+    const tododelete = document.createElement("button")
+    tododelete.classList.add("btn")
+    tododelete.classList.add("btn-danger")
+    tododelete.classList.add("m-1")
+    tododelete.innerHTML =`<i class="fa-solid fa-trash"></i>`
+    todoLi.appendChild(tododelete)
+
     todoResult.appendChild(todoLi)
 
-    //todoInput.value=''
+    todoInput.value=''
+}
+
+// function toggleToDo(e){
+//     const item = e.target
+
+//     const main_item = item.parentElement.parentElement
+
+//      if (main_item.classList[3] == 'completed'){
+//       main_item.classList.remove('completed')
+//      }else{
+//         main_item.classList.add('completed')
+//      }
+// }
+ 
+function taskComplete(e){
+
+}
+
+function taskDelete(e){
+    
 }
 
 
 
 
-
 todoBtn.addEventListener('click' ,addTodo)
+// todoResult.addEventListener('click',toggleToDo)
